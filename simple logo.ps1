3ebf5a6e983135825aecb93985b05df09a5dec3c
Alex Wittig <alexandervvittig@users.noreply.github.com>
2018-10-15 13:21:41 -0700

    Add files via upload

---
+++ simple logo.ps1
@@ -0,0 +1,10 @@
+Clear-Host
+$dersys_logo = @'
+ ___  ___ ___  _____   _____   ___ ___  __  __ 
+|   \| __| _ \/ __\ \ / / __| / __/ _ \|  \/  |
+| |) | _||   /\__ \\ V /\__ \| (_| (_) | |\/| |
+|___/|___|_|_\|___/ |_| |___(_)___\___/|_|  |_|
+'@
+
+$dersys_logo | write-host -ForegroundColor red
+Write-Host ">> Logscraper - Version 3.4" -ForegroundColor Yellow -BackgroundColor Black
